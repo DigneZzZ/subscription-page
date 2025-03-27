412bcb09e6103d9d8a463ab19ce564d85c3e6d71
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2025-03-28 00:15:20 +0300

    Update app.types.ts

--- client/src/shared/types/app.types.ts
+++ client/src/shared/types/app.types.ts
@@ -2,7 +2,6 @@ export interface AppConfig {
     id: string;
     name: string;
     priority: number;
-    url?: string;
     urlScheme: string;
     platform: 'ios' | 'android' | 'desktop';
     description: string;
