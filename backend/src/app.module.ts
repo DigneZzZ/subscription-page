e08a4605f20c3d6583036f9089bbf6180258b1df
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2026-04-23 02:04:59 +0300

    feat: register CardLinkModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 
 import { validateEnvConfig } from '@common/utils/validate-env-config';
 import { configSchema, Env } from '@common/config/app-config';
+import { CardLinkModule } from '@common/cardlink/cardlink.module';
 import { PlategaModule } from '@common/platega/platega.module';
 import { AxiosModule } from '@common/axios/axios.module';
 import { WataModule } from '@common/wata/wata.module';
@@ -14,6 +15,7 @@ import { SubscriptionPageBackendModule } from '@modules/subscription-page-backen
         AxiosModule,
         WataModule,
         PlategaModule,
+        CardLinkModule,
         ConfigModule.forRoot({
             isGlobal: true,
             cache: true,
